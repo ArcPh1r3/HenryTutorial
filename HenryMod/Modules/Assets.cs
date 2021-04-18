e450e697bc37b7b9dcd9b70bb559e8271242fa78
Robert <arcph1r3@gmail.com>
2021-04-18 18:09:34 -0400

    typo fix

--- HenryMod/Modules/Assets.cs
+++ HenryMod/Modules/Assets.cs
@@ -37,7 +37,7 @@ namespace HenryMod.Modules
 
         internal static void Initialize()
         {
-            if (assetbundleName == "myassetbundlee")
+            if (assetbundleName == "myassetbundle")
             {
                 Debug.LogError("AssetBundle name hasn't been changed- not loading any assets to avoid conflicts");
                 return;
